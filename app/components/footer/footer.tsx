cb0ec2dfc6dc15f6c10d62982bd88baf134525ba
Damien Bigot <damienbigot@Damiens-iMac-Home.local>
2024-05-20 09:01:56 +0900

    update footer component

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import styles from './footer.module.css';
+import Image from 'next/image';
 
 const Footer = () => {
   return (
-    <div>
+    <div className={styles.container}>
         <div>©2024 Damien Bigot. All rights reserved.</div>
+        <div className={styles.social}>
+          <Image src="/1.png" width={15} height={15} alt=""/>
+          <Image src="/2.png" width={15} height={15} alt=""/>
+          <Image src="/3.png" width={15} height={15} alt=""/>
+          <Image src="/4.png" width={15} height={15} alt=""/>
+        </div>
     </div>
   );
 }
