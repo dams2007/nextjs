a2893ce4dbea6291f716242384cbf0125e08daed
Damien Bigot <damien.bigot@gmail.com>
2024-05-25 15:15:47 +0900

    update blog id type

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -17,8 +17,9 @@ export type RawDataDetailBlogPost = {
 };
 
 const BlogPost = async ({ params }: propsBlogPost) => {
+	const id = params.id as string;
 	const data = (await get(
-		`http://localhost:3000/api/posts/${params.id}`
+		`http://localhost:3000/api/posts/${id}`
 	)) as RawDataDetailBlogPost;
 
 	return (
