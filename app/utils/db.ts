bc81fb9a1b30d2cd245222270fe6f7b0934d3935
Damien Bigot <damien.bigot@gmail.com>
2024-05-25 20:33:41 +0900

    update variables

--- app/utils/db.ts
+++ app/utils/db.ts
@@ -10,3 +10,28 @@ const connect = async () => {
 };
 
 export default connect;
+
+// import { NextApiRequest, NextApiResponse } from "next";
+// import { MongoClient } from "mongodb";
+
+// const uri = process.env.MONGODB_URI as string; // Use MONGODB_URI environment variable
+
+// const client = new MongoClient(uri);
+
+// export default async function handler(
+// 	req: NextApiRequest,
+// 	res: NextApiResponse
+// ) {
+// 	try {
+// 		await client.connect();
+// 		const database = client.db("test"); // Replace with your database name
+// 		const collection = database.collection("posts"); // Replace with your collection name
+// 		const posts = await collection.find({}).toArray();
+
+// 		res.status(200).json(posts);
+// 	} catch (error) {
+// 		res.status(500).json({ error: "Failed to fetch data" });
+// 	} finally {
+// 		await client.close();
+// 	}
+// }
